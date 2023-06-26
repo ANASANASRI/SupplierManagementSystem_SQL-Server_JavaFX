bf4e2d83f04cac4461b32a132ab610e3ff743eca
ANAS ANASRI <100525138+ANASRIANAS@users.noreply.github.com>
2023-06-26 13:56:55 +0100

    Inscription first commit

--- src/main/java/com/afm/suppliermanagementsystem/controller/Inscription.java
+++ src/main/java/com/afm/suppliermanagementsystem/controller/Inscription.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 import com.afm.suppliermanagementsystem.dao.imp.DB;
 import com.afm.suppliermanagementsystem.model.Compte;
+import com.afm.suppliermanagementsystem.services.CompteService;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 
@@ -17,13 +18,13 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Inscription {
-
-	// Les variables interface graphique
 	public int type;
 	public static Compte profile;
 
@@ -36,6 +37,10 @@ public class Inscription {
 	@FXML
 	private JFXTextField adresse;
 
+	@FXML
+	private JFXTextField identifiant;
+	@FXML
+	private PasswordField mot_pass;
 	@FXML
 	private JFXTextField id;
 
@@ -75,4 +80,48 @@ public class Inscription {
 		DB.getConnection();
 		loadCon(e);
 	}
+
+	@FXML
+	public void inscrirenow(ActionEvent e) throws IOException {
+			// Get the values from the input fields
+			String nomValue = nom.getText();
+			String prenomValue = prenom.getText();
+			String telValue = tel.getText();
+			String pseudoValue = pseudo.getText();
+			String identifiantValue = identifiant.getText();
+			String motPassValue = mot_pass.getText();
+			String adresseValue = adresse.getText();
+
+			// Perform validation if needed
+
+			// Create a new Compte object
+			Compte compte = new Compte(nomValue, prenomValue, telValue, pseudoValue, identifiantValue, motPassValue, adresseValue , 1);
+
+			// Save the Compte object using the CompteService
+			CompteService compteService = new CompteService();
+			compteService.save(compte);
+
+		if (compteService.findCompte(nomValue, motPassValue)) {
+
+			Parent root = FXMLLoader.load(getClass().getResource("/com/afm/suppliermanagementsystem/fxml/MenuAdmis.fxml"));
+			Scene s = new Scene(root);
+			Stage fenetre = (Stage) ((Node) e.getSource()).getScene().getWindow();
+			fenetre.setScene(s);
+			fenetre.setResizable(false);
+			Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+			fenetre.setX((primScreenBounds.getWidth() - fenetre.getWidth()) / 2);
+			fenetre.setY((primScreenBounds.getHeight() - fenetre.getHeight()) / 4);
+
+			fenetre.show();
+		} else {
+			// Show an alert for unsuccessful connection
+			Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+			errorAlert.setTitle("Connection Error");
+			errorAlert.setHeaderText(null);
+			errorAlert.setContentText("Failed to connect! Invalid username or password.");
+			errorAlert.showAndWait();
+		}
+
+		}
 }
+
--- src/main/java/com/afm/suppliermanagementsystem/services/CompteService.java
+++ src/main/java/com/afm/suppliermanagementsystem/services/CompteService.java
@@ -2,6 +2,8 @@ package com.afm.suppliermanagementsystem.services;
 
 import com.afm.suppliermanagementsystem.dao.CompteDao;
 import com.afm.suppliermanagementsystem.dao.imp.CompteDaoImp;
+import com.afm.suppliermanagementsystem.model.Compte;
+import com.afm.suppliermanagementsystem.model.Fournisseur;
 
 public class CompteService {
     private static CompteDao compteDao = new CompteDaoImp();
@@ -11,5 +13,10 @@ public class CompteService {
     public static Boolean findCompte(String nom, String password) {
         return compteDao.findCompte(nom, password);
     }
+
+    public void save(Compte compte) {
+        compteDao.insert(compte);
+    }
+
 
 }
