a6dbd3553264f4c62b0c36093e7b59c88624b331
ANAS ANASRI <100525138+ANASRIANAS@users.noreply.github.com>
2023-08-15 10:15:48 +0100

    Clean additional files and generate JAR file

--- src/main/java/com/afm/suppliermanagementsystem/controller/Inscription.java
+++ src/main/java/com/afm/suppliermanagementsystem/controller/Inscription.java
@@ -17,8 +17,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.PasswordField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,12 @@ module com.afm.suppliermanagementsystem {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
-    requires java.sql;
     requires com.jfoenix;
     requires itextpdf;
     requires javafx.web;
     requires org.apache.pdfbox;
     requires java.desktop;
+    requires java.sql;
 
     opens com.afm.suppliermanagementsystem to javafx.fxml;
     exports com.afm.suppliermanagementsystem;
